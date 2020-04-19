94c8c0950956fb61e524233b139d107364edec38
SmallFactory <admin@smallfactorystudios.com>
2020-04-19 14:06:05 -0700

    refactor the dataLoading classes

--- GitHubUsers/Controllers/FavoritesController.swift
+++ GitHubUsers/Controllers/FavoritesController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FavoritesController: UIViewController {
+class FavoritesController: DataLoadingController {
 	
 	let tableView = UITableView()
 	var favorites: [Follower] = []
